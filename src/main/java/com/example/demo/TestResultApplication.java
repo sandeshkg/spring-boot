aaa82c7b5bf0679108e547414a6afeb09e02900c
Sandesh Kumar <dev.skg@gmail.com>
2017-11-15 21:37:40 +0530

    bug

--- src/main/java/com/example/demo/TestResultApplication.java
+++ src/main/java/com/example/demo/TestResultApplication.java
@@ -1,14 +1,18 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -86,7 +90,18 @@ class SampleCLR implements CommandLineRunner{
 	}
 }
 
+@RefreshScope
+@RestController
+class HelloMessageController{
 
+	@Value("${message}")
+	private String msg;
+
+	@RequestMapping("/message")
+	String message() {
+		return this.msg;
+	}
+}
 
 
 
