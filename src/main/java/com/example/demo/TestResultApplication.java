f8dc5010f554f3f8d7dbe994337c20b2238b13c1
Sandesh Kumar <dev.skg@gmail.com>
2017-11-18 13:49:50 +0530

    enable registration with eureka

--- src/main/java/com/example/demo/TestResultApplication.java
+++ src/main/java/com/example/demo/TestResultApplication.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
@@ -20,6 +21,7 @@ import javax.persistence.Id;
 import java.util.Collection;
 import java.util.stream.Stream;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class TestResultApplication {
 
